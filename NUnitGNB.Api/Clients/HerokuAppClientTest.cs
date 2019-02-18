2464730dc17291063911ec593ef430e336775e1b
Henk Sandoval <henk.sandoval@hotmail.com>
2019-02-17 21:22:28 -0400

    Settings unit tests

---
+++ NUnitGNB.Api/Clients/HerokuAppClientTest.cs
@@ -0,0 +1,37 @@
+﻿using GNB.Api.Clients;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace GNB.Api.Tests.Clients
+{
+    [TestFixture]
+    public class HerokuAppClientTest
+    {
+        private HerokuAppClient HerokuAppClient { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            HerokuAppClient = new HerokuAppClient(new HttpClient
+            {
+                BaseAddress = new Uri("http://quiet-stone-2094.herokuapp.com")
+            });
+        }
+
+        [Test]
+        public async Task GetRates()
+        {
+            Stream result = await HerokuAppClient.GetStreamRates();
+        }
+
+        [Test]
+        public void Test()
+        {
+            bool a = true;
+            Assert.IsTrue(a);
+        }
+    }
+}
