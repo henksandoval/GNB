d99082e5074f4b97eea67112a399c828b1650a61
Henk Sandoval <henk.sandoval@hotmail.com>
2019-02-18 01:43:33 -0400

    Refactoring HerokuAppClientTest

--- NUnitGNB.Api/Clients/HerokuAppClientTest.cs
+++ NUnitGNB.Api/Clients/HerokuAppClientTest.cs
@@ -21,17 +21,25 @@ namespace GNB.Api.Tests.Clients
         }
 
         [Test]
-        public async Task GetRates()
+        public async Task GetStringRatesIsNotNull()
         {
             string rates = await HerokuAppClient.GetStringRates();
             Assert.IsNotNull(rates);
         }
 
         [Test]
-        public async Task GetTransactions()
+        public async Task GetStringTransactionsIsNotNull()
         {
             string transactions = await HerokuAppClient.GetStringTransactions();
             Assert.IsNotNull(transactions);
+        }
+
+        [TestCase(arg1: 2, arg2: 2, arg3: 4, TestName = "2 + 2 = 4")]
+        [TestCase(arg1: 2, arg2: 4, arg3: 6, TestName = "2 + 4 = 6")]
+        [TestCase(arg1: 2, arg2: -1, arg3: 1, TestName = "2 + (-1) = 1")]
+        public void Prueba(int a, int b, int r)
+        {
+            Assert.AreEqual(a + b, r);
         }
     }
 }
