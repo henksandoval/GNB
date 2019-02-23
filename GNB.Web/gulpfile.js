4c6fc09853617236289b8de3b44cf3abf3b505a7
Henk Sandoval <henk.sandoval@hotmail.com>
2019-02-23 00:08:34 -0400

    project working with gulpfile and npm

--- GNB.Web/gulpfile.js
+++ GNB.Web/gulpfile.js
@@ -9,54 +9,11 @@ const dependencies = {
     'bootstrap': {
         'dist/**/*': ''
     },
-    'animate.css': {
-        '/animate.*': '',
-    },
-    'pace-js': {
-        '/pace.*': '',
-    },
-    'jquery-slimscroll': {
-        '/jquery.*': '',
-    },
-    'js-cookie': {
-        'src/*' : '',
-    },
-    'select2': {
-        'dist/**/*': '',
-    },
-    'select2-theme-bootstrap4': {
-        'dist/*': ''
-    },
-    'bootstrap4-datetimepicker': {
-        'build/**/*': '',
-    },
-    'moment': {
-        'min/*': '',
-    },
     'datatables.net': {
-        'js/*': '',
-    },
-    'datatables.net-bs4': {
-        'css/*': 'css',
-        'js/*': 'js'
-    },
-    'datatables.net-buttons': {
         'js/*': '',
     },
     'sweetalert2': {
         'dist/*': '',
-    },
-    'underscore': {
-        'underscore*': '',
-    },
-    'jstree': {
-        'dist/**/*': '',
-    },
-    'fullcalendar': {
-        'dist/**/*': '',
-    },
-    'switchery': {
-        'standalone/*': '',
     },
     'jquery-validation': {
         'dist/*': '',
@@ -64,15 +21,13 @@ const dependencies = {
     'jquery-validation-unobtrusive': {
         'dist/*': '',
     },
-    'morris.js': {
-        'morris.*': '',
-    },
-    'raphael': {
-        'raphael.*': '',
+    'perfect-scrollbar': {
+        'dist/*': 'js',
+        'css/*': 'css'
     },
 };
 
-const pathLibrary = 'wwwroot/lib/thirdPartyLibrary';
+const pathLibrary = 'wwwroot/lib/thirdPartyLibrary/';
 
 gulp.task('clean', function (cb) {
     console.log(`Clean folder ${pathLibrary}`);
@@ -85,7 +40,7 @@ gulp.task('transport', function () {
     for (var dependency in dependencies) {
         for (var itemdependency in dependencies[dependency]) {
             let source = `node_modules/${dependency}/${itemdependency}`;
-            let destinationFolder = `wwwroot/lib/${dependency.replace('@','')}/${dependencies[dependency][itemdependency]}`;
+            let destinationFolder = `${pathLibrary}${dependency.replace('@','')}/${dependencies[dependency][itemdependency]}`;
             console.log(`Transfer library ${dependency} from ${source} to ${destinationFolder}`);
             streams.push(gulp.src(source)
                 .pipe(gulp.dest(destinationFolder)));
